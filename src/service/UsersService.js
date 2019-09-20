0216c77b402b7fa133dfe086bc128b251771ad18
Rafael Pazini <rflpazini@gmail.com>
2019-09-20 15:36:27 -0300

    fix: lint

--- src/service/UsersService.js
+++ src/service/UsersService.js
@@ -3,47 +3,47 @@ const UserHelper = require('../utils/UserHelper');
 
 class UsersService {
   static async create(req, res, next) {
-      const { email, password } = req.body;
+    const { email, password } = req.body;
 
-      if (!email || !password) {
-        console.info(`validadeUser(${email}) >> Error: email or password null`);
+    if (!email || !password) {
+      console.info(`validadeUser(${email}) >> Error: email or password null`);
 
-        return res
-          .status(400)
-          .json({ message: 'Email and password must be provided' });
-      }
+      return res
+        .status(400)
+        .json({ message: 'Email and password must be provided' });
+    }
 
-      if (!UserHelper.isValidEmail(email)) {
-        console.info(`validadeUser(${email}) >> Error: Invalid email`);
+    if (!UserHelper.isValidEmail(email)) {
+      console.info(`validadeUser(${email}) >> Error: Invalid email`);
 
-        return res.status(400).json({
-          message: `Please provide a valid email...`,
-        });
-      }
+      return res.status(400).json({
+        message: `Please provide a valid email...`,
+      });
+    }
 
-      const user = await Users.create(email, password);
-      if (!user) {
-        return res.status(400).json({ message: 'User already exists' });
-      }
+    const user = await Users.create(email, password);
+    if (!user) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
 
-      return res.status(201).json(user);
+    return res.status(201).json(user);
   }
 
   static async findByEmail(req, res, next) {
-      const email = req.query.email;
+    const email = req.query.email;
 
-      const user = await Users.findByEmail(email);
-      if (!user) {
-        return res.status(404).json({ message: `User ${email} not found...` });
-      }
+    const user = await Users.findByEmail(email);
+    if (!user) {
+      return res.status(404).json({ message: `User ${email} not found...` });
+    }
 
-      return res.status(200).json(user);
+    return res.status(200).json(user);
   }
 
   static async findAll(req, res, next) {
-      const users = await Users.findAll();
+    const users = await Users.findAll();
 
-      return res.status(200).json(users);
+    return res.status(200).json(users);
   }
 
   static async deleteUser(req, res, next) {
